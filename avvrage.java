ae528a0f8a7bcd2d3d5ad23d8c299525b49c9d11
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-02-01 11:11:04 +0530

    Added a new file

---
+++ avvrage.java
@@ -0,0 +1,10 @@
+public class avvrage {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 35;
+        int c = 30;
+        float avg = (a+b+c)/3;
+        System.out.println(avg);
+    }
+
+}
