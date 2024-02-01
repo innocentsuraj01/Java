38fc31f083a553520561976804d54719a329ac44
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-02-01 11:36:13 +0530

    Added a new line

--- avvrage.java
+++ avvrage.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class avvrage {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 35;
-        int c = 30;
-        float avg = (a+b+c)/3;
+        Scanner var = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int num1 = var.nextInt();
+        System.out.println("Enter Second Number");
+        int num2 = var.nextInt();
+        System.out.println("Enter Third Number");
+        int num3 = var.nextInt();
+        float avg = (float) (num1 + num2 + num3)/3;
         System.out.println(avg);
+        var.close();
+    
     }
 
 }
